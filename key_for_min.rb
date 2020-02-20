62b2e45f65999b14faa06565caf295aa56d764b0
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-20 18:38:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,25 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
+
+
   name_hash.map do |name, num|
-  if num <= 1
-    return name
-  elsif num == 10
-   return name
- else
-     nil
-   end
+binding.pry
   end
 end
+
+# 
+# def key_for_min_value(name_hash)
+#   name_hash.map do |name, num|
+#   if num <= 1
+#     return name
+#   elsif num == 10
+#    return name
+#  else
+#      nil
+#    end
+#   end
+# end
